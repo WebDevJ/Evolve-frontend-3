a8cec5187461f93b9bd1093a70b92fca7cb0c371
WebDevJ <WebDevJ@users.noreply.github.com>
2024-09-24 12:40:03 -0400

    TODO: add .env for api key

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 // import * as firebase from 'firebase'
 //   // Initialize Firebase
 //   var config = {
-//     apiKey: "AIzaSyAmMJK7dZtkw0W5YLW8Dp9UFGS1JtvxUcw",
+// api key vis .env
 //     authDomain: "evolve-1.firebaseapp.com",
 //     databaseURL: "https://evolve-1.firebaseio.com",
 //     projectId: "evolve-1",
